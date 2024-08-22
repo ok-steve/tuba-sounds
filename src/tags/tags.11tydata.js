f44190fe96916d405c81baa21404a6446ea07e2e
Steve Cherry <2136364+ok-steve@users.noreply.github.com>
2024-08-22 06:23:29 -0400

    Add tags pages

---
+++ src/tags/tags.11tydata.js
@@ -0,0 +1,11 @@
+module.exports = function () {
+  return {
+    getTagsFromCollection(collection) {
+      return Array.from(
+        new Set(
+          collection.flatMap((item) => item.data.tags).filter((item) => item)
+        )
+      );
+    },
+  };
+};
